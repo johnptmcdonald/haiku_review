93ab6b777e80914c83dcc72c49e106ee48f41659
johnptmcdonald <johnptmcdonald@gmail.com>
2014-10-06 16:55:55 -0700

    log in and out functionality added with session method and permanent cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	def create
  		@user = User.new(user_params)
   		if @user.save
+  			log_in @user
   			flash[:success] = "Welcome to this awesome app!"
   			redirect_to @user
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :users
 
   get '/signup', to: 'users#new'
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
