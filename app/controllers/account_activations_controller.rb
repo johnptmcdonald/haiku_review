4fca798bc4f05c26a74c62e77e58b4c9f851b942
johnptmcdonald <johnptmcdonald@gmail.com>
2014-10-13 19:01:32 -0700

    removed account activation bug from activations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,7 @@
 class AccountActivationsController < ApplicationController
 	def edit
   		user = User.find_by(email: params[:email])
-  		if user && !user.activated! && user.authenticated?(:activation, params[:id])
+  		if user && !user.activated? && user.authenticated?(:activation, params[:id])
 			user.activate
       		flash[:success] = "Account activated!"
       		log_in user
