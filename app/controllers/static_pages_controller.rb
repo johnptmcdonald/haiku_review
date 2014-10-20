3a79b726df3b986720f17b0e310fa2a599322777
johnptmcdonald <johnptmcdonald@gmail.com>
2014-10-20 16:21:13 -0400

    un-logged in users can still see all the most recent haiku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,9 @@ class StaticPagesController < ApplicationController
   	if logged_in?
 	  	@micropost = current_user.microposts.build if logged_in?
 	  	@feed_items = current_user.feed.paginate(page: params[:page])
-	end
+
+	  end
+    @microposts = Micropost.all
   end
 
   def help
